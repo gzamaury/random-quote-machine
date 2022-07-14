b6b88830fd45d1df2627fbbe985bf392ea98fb1e
Amaury Gz <gzamaury@hotmail.com>
2022-07-14 14:42:48 -0500

    Add: Quote API

---
+++ src/api/quoteApi.js
@@ -0,0 +1,13 @@
+import client from "./client";
+
+const config = {
+  headers: {
+    "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_MY_EXPOSED_KEY,
+  },
+};
+
+const fetchRandomQuote = () =>
+  client.get("https://quotes15.p.rapidapi.com/quotes/random/", config);
+
+export default fetchRandomQuote;
