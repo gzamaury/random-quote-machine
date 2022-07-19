9350b8b366c494ac290229e7bf23d72eab9c05ab
Amaury Gz <gzamaury@hotmail.com>
2022-07-18 20:50:32 -0500

    Add: Tweet button - RQM-28 RQM-33 (Jira User Stories)

--- src/component/QuoteBox/QuoteBox.js
+++ src/component/QuoteBox/QuoteBox.js
@@ -2,15 +2,28 @@ import "./QuoteBox.css";
 import PropTypes from "prop-types";
 import quote from "./quote.svg";
 import NextBtn from "../NextBtn/NextBtn";
+import twitter from "./twitter.svg";
 
 function QuoteBox({ text, author, onClickNextBtn }) {
+  const tweetHref = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    `${text} \n\t—${author}`
+  )}`;
+
   return (
     <div id="quote-box" className="quoteBox">
       <div className="quoteBox-container row-layout">
         <div id="quote-col" className="column-layout">
           <img src={quote} className="quote-img" alt="quote" />
           <p id="text" data-testid="quote-box">
-            {text}
+            {text}{" "}
+            <a
+              id="tweet-quote"
+              href={tweetHref}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={twitter} alt="twitter" />
+            </a>
           </p>
           <span id="author">— {author}</span>
         </div>
